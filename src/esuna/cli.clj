6cdb7b8084fd2ef5c85f102a0a3a62be3c6cba45
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-06-02 10:20:38 +0200

    add cli for dispatch + helper function

---
+++ src/esuna/cli.clj
@@ -0,0 +1,28 @@
+(ns esuna.cli
+  "handles cli args and dispatching to internal functions"
+  (:require [babashka.cli :as cli]
+            [clojure.string :as str]
+            [esuna.api :as api]))
+
+(defn- format-commands-for-print [m]
+  (let [cmds (->> (map :cmds m)
+                  (filter not-empty)
+                  (map #(str/join " " %))
+                  sort)
+        max-cmd-width (apply max (map count cmds))
+        fmt-string (str "  %-" max-cmd-width "s")]
+    (mapv #(format fmt-string %) cmds)))
+
+(defn- print-help [m]
+  (let [cmds (str/join "\n" (format-commands-for-print m))]
+    (println "Usage: esu [cmd] ([sub-cmd])
+
+Subcommands:
+ ")
+    (println cmds)))
+
+(def table [{:cmds ["repo" "clone"] :fn api/clone-repos}
+            {:cmds [] :fn (fn [_] (print-help table))}])
+
+(defn -main [& args]
+  (cli/dispatch table args {}))
